01ab45e571cf9da2ccfb98e9dc13c637ce86e0fa
Patrimad <70513116+Patrimad@users.noreply.github.com>
2026-03-12 01:53:01 +0100

    InitialPush

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerMovement : MonoBehaviour
+{
+    public float force = 10f;
+    public Transform cameraTransform;
+
+    private Rigidbody rb;
+    private Vector2 moveInput;
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void OnMove(InputValue value)
+    {
+        moveInput = value.Get<Vector2>();
+    }
+
+    void FixedUpdate()
+    {
+        Vector3 camForward = cameraTransform.forward;
+        Vector3 camRight = cameraTransform.right;
+
+        camForward.y = 0;
+        camRight.y = 0;
+
+        camForward.Normalize();
+        camRight.Normalize();
+
+        Vector3 moveDirection = camForward * moveInput.y + camRight * moveInput.x;
+
+        rb.AddForce(moveDirection * force, ForceMode.Acceleration);
+    }
+}
